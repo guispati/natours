df933467de61cbce5ff58fe9962e5ee8113734f6
Guilherme Spati <guilherme.spati723@gmail.com>
2022-09-29 19:24:53 -0300

    calculate average ratings on tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -30,6 +31,8 @@ const reviewSchema = new mongoose.Schema({
     toObject: { virtuals: true },
 });
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function(next) {
     // this.populate({
     //     path: 'tour',
@@ -47,6 +50,40 @@ reviewSchema.pre(/^find/, function(next) {
     next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId },
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' },
+            },
+        },
+    ]);
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating,
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5,
+        });
+    }
+}
+
+reviewSchema.post('save', function() {
+    this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.post(/^findOneAnd/, async function(doc) {
+    await doc.constructor.calcAverageRatings(doc.tour);
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
