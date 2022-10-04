0dbded801f9c45a885f2d52ac32b77190c42071b
Guilherme Spati <guilherme.spati@uel.br>
2022-10-03 23:57:09 -0300

    finish section 12

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -22,29 +22,50 @@ const handleValidationErrorDB = err => {
     return new AppError(message, 400);
 }
 
-const sendErrorDev = (err, res) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack,
+const sendErrorDev = (err, req, res) => {
+    if (req.originalUrl.startsWith('/api')) {
+        return res.status(err.statusCode).json({
+            status: err.status,
+            error: err,
+            message: err.message,
+            stack: err.stack,
+        });
+    }
+
+    return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong!',
+        msg: err.message,
     });
 }
 
-const sendErrorProd = (err, res) => {
+const sendErrorProd = (err, req, res) => {
+    if (req.originalUrl.startsWith('/api')) {
+        if (err.isOperational) {
+            return res.status(err.statusCode).json({
+                status: err.status,
+                message: err.message,
+            });
+        }
+        console.error('ERROR', err);
+
+        return res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!',
+        });
+    }
     if (err.isOperational) {
-        res.status(err.statusCode).json({
+        return res.status(err.statusCode).json({
             status: err.status,
             message: err.message,
         });
-    } else {
-        console.error('ERROR', err);
-
-        res.status(500).json({
-            status: 'error',
-            message: 'Something went very wrong!',
-        })
     }
+
+    console.error('ERROR', err);
+
+    return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong!',
+        msg: 'Please try again later',
+    });
 }
 
 module.exports = (err, req, res, next) => {
@@ -55,9 +76,10 @@ module.exports = (err, req, res, next) => {
     err.status = err.status || defaultErrorStatus;
 
     if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(err, res);
+        sendErrorDev(err, req, res);
     } else if (process.env.NODE_ENV === 'production') {
         let error = { ...err };
+        error.message = err.message;
         
         if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicatedFieldsDB(error);
@@ -65,6 +87,6 @@ module.exports = (err, req, res, next) => {
         if (error.name === 'JsonWebTokenError') error = handleJWTError();
         if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
-        sendErrorProd(error, res);
+        sendErrorProd(error, req, res);
     }
 }
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync')
 
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -16,6 +17,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
         fields: 'review rating user',
     });
 
+    if (!tour) {
+        return next(new AppError('There is no tour with that name.', 404));
+    }
+
     res.set(
         'Content-Security-Policy',
         "default-src 'self' https://*.mapbox.com; base-uri 'self'; block-all-mixed-content; font-src 'self' https:; frame-ancestors 'self'; img-src 'self' blob: data:; object-src 'none'; script-src 'unsafe-inline' https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob:; style-src 'self' https: 'unsafe-inline'; upgrade-insecure-requests;"
@@ -29,5 +34,11 @@ exports.getTour = catchAsync(async (req, res, next) => {
 exports.getLoginForm = (req, res) => {
     res.status(200).render('login', {
         title: 'Log into your account',
+    });
+}
+
+exports.getAccount = (req, res) => {
+    res.status(200).render('account', {
+        title: 'Your account',
     });
 }
